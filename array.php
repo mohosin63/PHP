e30b3e6bc283bbcd6e8c9b85515603b9850a42f4
mohosin63 <60107277+mohosin63@users.noreply.github.com>
2021-02-23 13:05:11 +0600

    array

---
+++ array.php
@@ -0,0 +1,12 @@
+<?php
+
+$divisions = array("Dha" , "Raj" , "khu" , "Syl" , "Bar");
+echo "<pre>";
+print_r($divisions);
+$country['Bangladesh'] = 'Dhaka';
+$country['Srilanka'] = 'Colomo';
+$country['Austrila'] = 'Sydney';
+$country['England'] = 'London';
+$country['USA'] = 'Washington';
+print_r($country);
+ ?>
