865ff6aab21d1ecdcbd1b0f4bd74aad7d2a29dac
mohosin63 <60107277+mohosin63@users.noreply.github.com>
2021-02-23 13:16:08 +0600

    typpe_identy

---
+++ typpe_identy.php
@@ -0,0 +1,25 @@
+<?php 
+
+// $item = "43";
+// if (is_integer($item)){
+// 	echo "Yes";
+// }
+?>
+
+
+<?
+// $x = null;
+// echo (is_null($x))? 'Yes Null': 'No';
+?>
+
+<?
+// $x = 0;
+// $r = is_bool($x);
+// echo $r;
+?>
+
+<?
+$x = [10, 20, 30];
+$r = is_array($x);
+echo $r;
+?>
