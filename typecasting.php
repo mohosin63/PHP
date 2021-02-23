3326b6611d83280a3686401f01a3ba33d6a9db9b
mohosin63 <60107277+mohosin63@users.noreply.github.com>
2021-02-23 13:15:44 +0600

    typecasting

---
+++ typecasting.php
@@ -0,0 +1,41 @@
+<?
+// $score = (double) 13;
+// //var_dump($score);
+// $res = $scroe + 54.5;
+// echo $res;
+?>
+
+<?
+// $score = (int)14.8;
+// var_dump($score);
+?>
+
+<?
+// $sentence = "This is a sentence";
+// $changed = (int)$sentence;
+// var_dump($changed);
+?>
+
+<?
+// $score = 1114;
+// $socreboard = (array) $score;
+// echo $socreboard[0];
+// echo "<br>";
+// var_dump($socreboard);
+
+?>
+
+<?
+
+// $model = "Toyota";
+// $obj = (object)$model;
+// var_dump($obj);
+// echo $obj->scalar;
+?>
+
+<?
+
+$x = 5800;
+$convert = (string) $x;
+var_dump($convert);
+?>
