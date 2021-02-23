b0573227147f9cc8d85a4bf715a38f3e77b69f70
mohosin63 <60107277+mohosin63@users.noreply.github.com>
2021-02-23 13:07:11 +0600

    data_output

---
+++ data_output.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+//$tittle = "<h1>Output content</h1>";
+//$body = "<p>This content of paragrap...</p>";
+//echo $tittle , $body;
+//print  $tittle , $body; //syntax error
+//echo "{$tittle} {$body} Addisonal content goes here<br>";
+
+//variable can't give output using single code
+
+//echo '{$tittle} {$body} Addisonal content goes here';
+
+	?>
+
+
+	<?php 
+		$a =5;
+		//echo "{$a_abe}<br/>";
+		echo "{$a}_abc<br/>";
+	?>
+</body>
+</html>
