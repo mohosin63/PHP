96366069491466be009063ba6dcc16ba6efefffb
mohosin63 <60107277+mohosin63@users.noreply.github.com>
2021-02-24 11:56:04 +0600

    escape

---
+++ escape.php
@@ -0,0 +1,13 @@
+<?
+// $output = "This is one line.\n And this anther line";
+// echo $output;
+?>
+<?
+
+// $output1= "This \$animal is jump over the all.\n";
+// echo $output1;
+?>
+
+<?
+print 'This string will $print exactuly as it\'s \n declard.';
+?>
