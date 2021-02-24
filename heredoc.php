30295a2a08a8f26b2658bac2f7ef8f7a3a36e09c
mohosin63 <60107277+mohosin63@users.noreply.github.com>
2021-02-24 11:57:03 +0600

    heredoc

---
+++ heredoc.php
@@ -0,0 +1,9 @@
+<?
+$website = "https://www.roma.it";
+$output = <<<ALIM
+<p>We are taking tranin at IBB <a href = "$website">Roma</a> and TSP
+<i>TCL</i>.</p>
+ALIM;
+?>
+<hr>
+<div><? echo $output; ?></div>
