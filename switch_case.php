27e202669b8713f9c2c226fadccb7ce55fecc12b
mohosin63 <60107277+mohosin63@users.noreply.github.com>
2021-02-24 12:00:03 +0600

    switch_case

---
+++ switch_case.php
@@ -0,0 +1,20 @@
+<?
+$category = 7;
+switch ($category) {
+	case ($category <4):
+		echo "welcome";
+		break;
+
+		case ($category <=7):
+		echo "open";
+		break;
+
+		case ($category <10):
+		echo "well";
+		break;
+	
+	default:
+		echo "End";
+		break;
+}
+?>
